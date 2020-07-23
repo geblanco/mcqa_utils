0fa12d7c7b844af9562e448fd36d8a37037c0b35
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2020-07-23 15:49:39 +0200

    Bump version: 0.1.1 → 0.1.2

--- mcqa_utils/__init__.py
+++ mcqa_utils/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Guillermo E. Blanco"""
 __email__ = 'geblanco@lsi.uned.es'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/geblanco/mcqa_utils',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
