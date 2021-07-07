f3b73888c1da86df92c7e4553102a28dc97cbbba
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2021-07-07 11:30:48 +0200

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/geblanco/mcqa_utils',
-    version='0.2.2',
+    version='0.2.3',
     zip_safe=False,
 )
